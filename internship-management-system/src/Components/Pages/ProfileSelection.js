93ae510ded80d6f00bcd54c6ad651df8b2a312bf
deniz sun <deniz.sun@ug.bilkent.edu.tr>
2023-05-04 22:22:20 +0300

    Fixed notifs and login, added profile selection (not done yet)

---
+++ internship-management-system/src/Components/Pages/ProfileSelection.js
@@ -0,0 +1,48 @@
+import react, { Component } from 'react'
+import '../Styles/ProfileSelection.css'
+const userTypes = ["student",
+                            "instructor",
+                            "administrative-assistant",
+                            "TA",
+                            "coordinator",
+                            "admin" 
+                        ];
+function ProfileSelection() {
+  const [selectedUserType, setSelectedUserType] = useState('');
+
+  const handleUserTypeSelect = (userType) => {
+    setSelectedUserType(userType);
+  };
+
+  const handleUserTypeSubmit = () => {
+    fetch('/profile-selection', {
+      method: 'POST',
+      headers: {
+   //     'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `user-type=${selectedUserType}`
+    }).then(response => {
+      window.location.href = response.url;
+    });
+  };
+  return (
+    <div className="profile-selection-container">
+      <h1>Welcome, {/** name*/}
+
+      </h1>
+      <h1>Choose your account:</h1>
+      <div className="profile-selection-buttons-container">
+        <button className={`profile-selection-button ${selectedUserType === 'coordinator' ? 'selected' : ''}`} onClick={() => handleUserTypeSelect('coordinator')}>
+
+          <div>coordinator</div>
+        </button>
+        <button className={`profile-selection-button ${selectedUserType === 'instructor' ? 'selected' : ''}`} onClick={() => handleUserTypeSelect('instructor')}>
+          <div>instructor</div>
+        </button>
+      </div>
+      <button className="profile-selection-submit-button" disabled={!selectedUserType} onClick={handleUserTypeSubmit}>Go to profile</button>
+    </div>
+  );
+}
+
+export default ProfileSelection;
--- internship-management-system/src/Components/Pages/Statistics.js
+++ internship-management-system/src/Components/Pages/Statistics.js
@@ -1,24 +1,35 @@
-import React, {Component} from 'react'
-import '../Styles/Statistics.css'
-class Statistics extends Component{
-    
-    
-      render(){
-        return(
-            <div className='maincontainer'>
-                <div className='instructorstatus'>
+import React, { Component } from 'react';
+import '../Styles/Statistics.css';
 
-            hi
-                </div>
-                <div className='gradestatus'>
-hi
-                </div>
+class Statistics extends Component {
+  state = {
+    numStudents: 0,
+    numInstructors: 0
+  };
 
-            </div>
-         
-              
-        )}
+  componentDidMount() {
+    // Simulate fetching data from the server
+    setTimeout(() => {
+      this.setState({ numStudents: 37, numInstructors: 5 });
+    }, 1000);
+  }
 
+  render() {
+    const { numStudents, numInstructors } = this.state;
+
+    return (
+      <div className="maincontainer">
+        <div className="instructorstatus">
+          <h2>Instructor Status</h2>
+          <p>{numInstructors} instructor(s) have started grading reports</p>
+        </div>
+        <div className="gradestatus">
+          <h2>Report Upload Status</h2>
+          <p>{numStudents} student(s) have uploaded their reports</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Statistics
+export default Statistics;
