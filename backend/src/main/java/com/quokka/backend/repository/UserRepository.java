2874b0a479ffbd81da788e363ce153a227ffb8dd
emrekantas <91370821+emrekantas@users.noreply.github.com>
2023-05-05 18:36:35 +0300

    UserRepository imports + <class_names> added

--- backend/src/main/java/com/quokka/backend/repository/UserRepository.java
+++ backend/src/main/java/com/quokka/backend/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.quokka.backend.repository;
 
+import com.quokka.backend.models.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository{
+public interface UserRepository extends JpaRepository<UserProfile, Long>{
 
-        
+    
 }
