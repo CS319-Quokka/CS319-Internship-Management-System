d3dcf47e3f83bf5edae7cf61a26e7a49297464e7
Berkay Akkus <berkayakkus00@gmail.com>
2023-05-06 12:21:37 +0300

    berkay local branch deneme

--- backend/src/main/java/com/quokka/backend/repository/UserRepository.java
+++ backend/src/main/java/com/quokka/backend/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.quokka.backend.models.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// deneme
 @Repository
 public interface UserRepository extends JpaRepository<UserAccount,Long> {
 
