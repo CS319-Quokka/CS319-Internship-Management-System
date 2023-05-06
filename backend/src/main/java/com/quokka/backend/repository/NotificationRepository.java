857ebd14bc497b95ad2697834213b20f4b2df1fc
Berkay Akkus <berkayakkus00@gmail.com>
2023-05-06 17:22:42 +0300

    notification controller and repository added

---
+++ backend/src/main/java/com/quokka/backend/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.quokka.backend.repository;
+
+import com.quokka.backend.models.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification,Long> {
+}
