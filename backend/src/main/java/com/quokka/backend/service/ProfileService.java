54978bf444dd12b7a4df41c3bff2624ab6127092
melihhguvenn <melih.guven@ug.bilkent.edu.tr>
2023-05-09 23:07:28 +0300

    Exceptions of ProfileService and ProfileService structures are added

--- backend/src/main/java/com/quokka/backend/service/ProfileService.java
+++ backend/src/main/java/com/quokka/backend/service/ProfileService.java
@@ -1,4 +1,27 @@
 package com.quokka.backend.service;
 
+import com.quokka.backend.exception.StudentDoesNotExistException;
+import com.quokka.backend.models.Student;
+import com.quokka.backend.models.TeachingAssistant;
+import com.quokka.backend.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class ProfileService {
+    private final ProfileRepository profileRepository;
+
+    @Autowired
+    public ProfileService(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public void viewStudentInformation(Student student, Long profileId)throws StudentDoesNotExistException {
+        if (profileRepository.findById(profileId).get().getId() != student.getId()){
+            throw new StudentDoesNotExistException("Student with id " + student.getId() +
+                    " does not exist. It cannot be removed!");
+        }
+
+        //ToDo
+    }
 }
