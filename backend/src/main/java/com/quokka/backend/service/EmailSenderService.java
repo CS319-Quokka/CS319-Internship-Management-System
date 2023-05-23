dc8fa550566a67b1182896d714d5bb79b1b1e151
Yusuf Özyer <yusufozyer2@gmail.com>
2023-05-24 00:28:01 +0300

    mail sender implemented

--- backend/src/main/java/com/quokka/backend/service/EmailSenderService.java
+++ backend/src/main/java/com/quokka/backend/service/EmailSenderService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderService {
+    @Autowired
     private JavaMailSender javaMailSender;
 
     public void sendMail(String toEmail, String body, String subject) {
