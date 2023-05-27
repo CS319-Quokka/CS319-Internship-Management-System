b08c64108fcb4160ef29fdb7c9f034b234b75eac
deniz sun <deniz.sun@ug.bilkent.edu.tr>
2023-05-27 21:36:49 +0300

    TA feedback and evaluation pages are now different based on user role

--- internship-management-system/src/Components/Pages/StudentList.js
+++ internship-management-system/src/Components/Pages/StudentList.js
@@ -6,65 +6,64 @@ import { StudentData } from "../StudentData";
 import DisplayList from "./DisplayList";
 import "../Styles/StudentList.css";
 
-class StudentList extends Component {
 
-  
+class StudentList extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
-        studentNameList:[]
+        studentNameList:[],
+        linkName: ""
     }
-}
-
-
-componentDidMount() {
-    this.getAllStudents();
-}
-
-
-
-getAllStudents = async () =>{
-
-    const id = this.props.userId;
-
-    const response = await axios.get(`http://localhost:8080/get_all_users/${id}`);
-
-    const info = response.data[0]
-
-    const response2 = await axios.get(`http://localhost:8080/student?instructorId=${info.id}`);
-
-    const studentInfo = response2.data;
-    console.log(studentInfo.length);
-     var name = [];
-
-     for (var i = 0; i < studentInfo.length; i++) {
-    
-      var fullName = studentInfo[i].userAccount.firstName + " " + studentInfo[i].userAccount.lastName;
-      var studentType = studentInfo[i].role;
-      var courseCode = studentInfo[i].courseCode;
-      var grader = studentInfo[i].instructor.firstName;
-      var department = studentInfo[i].userAccount.department;
-      var studentStatus = studentInfo[i].status;
-      var companyForm = studentInfo[i].companyEvaluationForm;
-      var studentId = studentInfo[i].id
-    
-
-      name.push({
-        name: fullName,
-        type: studentType,
-        class: courseCode,
-        grader: grader,
-        department: department,
-        status: studentStatus,
-        form: companyForm,
-        id:studentId
-      });
+  }
+    componentDidMount() {
+        this.getAllStudents();
     }
+    getAllStudents = async () =>{
 
-     this.setState({ studentNameList: name });
-  
-}
+        const id = this.props.userId;
+
+        const response = await axios.get(`http://localhost:8080/get_all_users/${id}`);
+
+        const info = response.data[0]
+
+        const response2 = await axios.get(`http://localhost:8080/student?instructorId=${info.id}`);
+
+        const studentInfo = response2.data;
+         var name = [];
+
+         for (var i = 0; i < studentInfo.length; i++) {
+
+          var fullName = studentInfo[i].userAccount.firstName + " " + studentInfo[i].userAccount.lastName;
+          var studentType = studentInfo[i].role;
+          var courseCode = studentInfo[i].courseCode;
+          var grader = studentInfo[i].instructor.firstName;
+          var department = studentInfo[i].userAccount.department;
+          var studentStatus = studentInfo[i].status;
+          var companyForm = studentInfo[i].companyEvaluationForm;
+          var studentId = studentInfo[i].id
+
+
+          name.push({
+            name: fullName,
+            type: studentType,
+            class: courseCode,
+            grader: grader,
+            department: department,
+            status: studentStatus,
+            form: companyForm,
+            id:studentId
+          });
+        }
+
+
+         if (info.role === "Teaching Assistant") {
+             this.setState({ linkName: "/teachingassistantfeedback"});
+         }
+         else if (info.role === "Instructor") {
+             this.setState({linkName: "/evaluation"});
+         }
+         this.setState({ studentNameList: name });
+    }
 
   
   render() {
@@ -72,7 +71,7 @@ getAllStudents = async () =>{
     const history = this.props;
     return (
       <div className="student-list">
-        <DisplayList link = {"/evaluation"}  data={studentNameList} displayFields={['name','class', 'status']} />
+        <DisplayList link = {this.state.linkName}  data={studentNameList} displayFields={['name','class', 'status']} />
       </div>
     );
   }
